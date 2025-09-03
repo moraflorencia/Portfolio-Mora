ec9ea376787185ec241ab7156f7ff1fef7d5ca90
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-03 20:55:51 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -612,11 +612,11 @@ const projects = [
                       : 'bg-slate-800/80 hover:bg-slate-800 text-white'
                   }`}
                 >
-                  <<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m11.9 22l4.55-12h2.1l4.55 12H21l-1.075-3.05h-4.85L14 22zM4 19l-1.4-1.4l5.05-5.05q-.875-.875-1.588-2T4.75 8h2.1q.5.975 1 1.7t1.2 1.45q.825-.825 1.713-2.313T12.1 6H1V4h7V2h2v2h7v2h-2.9q-.525 1.8-1.575 3.7t-2.075 2.9l2.4 2.45l-.75 2.05l-3.05-3.125zm11.7-1.8h3.6l-1.8-5.1z"/></svg> 
+                  <Globe size={18} />
                 </button>
                 
                 <button
-                  onClick={toggleDarkMode}
+                  onClick={toggleDark Mode}
                   className={`p-2 rounded-xl transition-all duration-300 ${
                     isDarkMode 
                       ? 'bg-yellow-500 hover:bg-yellow-400 text-yellow-900' 
@@ -638,7 +638,7 @@ const projects = [
                     : 'bg-slate-800/80 hover:bg-slate-700 text-white'
                 }`}
               >
-                <<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m11.9 22l4.55-12h2.1l4.55 12H21l-1.075-3.05h-4.85L14 22zM4 19l-1.4-1.4l5.05-5.05q-.875-.875-1.588-2T4.75 8h2.1q.5.975 1 1.7t1.2 1.45q.825-.825 1.713-2.313T12.1 6H1V4h7V2h2v2h7v2h-2.9q-.525 1.8-1.575 3.7t-2.075 2.9l2.4 2.45l-.75 2.05l-3.05-3.125zm11.7-1.8h3.6l-1.8-5.1z"/></svg>
+                <Globe size={18} />
               </button>
               
               <button 
