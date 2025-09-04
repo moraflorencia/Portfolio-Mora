4cce52784566880e5a607bb1029913fc79084e1a
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-03 21:03:46 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -134,7 +134,7 @@ function App() {
       hero: {
         greeting: "Hello!",
         name: "Florencia Milagros Mora",
-        title: " Data Analyst",
+        title: "Data Analyst | Python · SQL · Power BI · Excel | Systems Engineering Student",
         subtitle: "Python · SQL · Power BI · Excel | Systems Engineering Student",
         description: "I'm excited to learn, grow as a team, and use data to create solutions with real impact.",
         downloadCV: "Download CV",
