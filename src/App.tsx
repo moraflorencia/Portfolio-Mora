4ea38b72be7774b3bad1bb32309a8856662dd12a
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-04 19:20:24 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { ChevronDown, Github, Linkedin, Mail, Phone, MapPin, ExternalLink, BarChart3, Database, Brain, Code, TrendingUp, BookOpen, Award, Briefcase, Send, Calendar, GraduationCap, X, ZoomIn, Moon, Sun, Menu, Globe, Star, Sparkles } from 'lucide-react';
 import { Languages } from 'lucide-react';
 import { FaWhatsapp } from "react-icons/fa";
- 
+  
     
 function App() {
   const [activeSection, setActiveSection] = useState('home'); 
