c8556e473f2467729a40d068fd76e1382386dfff
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-10 20:33:03 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -429,7 +429,7 @@ const projects = [
                   left: `${Math.random() * 100}%`,
                   top: `${Math.random() * 100}%`,
                   animationDelay: `${Math.random() * 3}s`,
-                  animationDuration: `${2 + Math.random() * 2}s`
+                  animationDuration: `${2 + Math.random() * 10}s`
                 }}
               >
                 <div 
