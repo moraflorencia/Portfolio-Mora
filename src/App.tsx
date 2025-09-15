633d5445d406d541e12eef5b7ae7f7f94b350a12
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-15 16:42:48 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1352,7 +1352,7 @@ const projects = [
 
       {/* Grid con 3 tarjetas */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 flex-1">
-        {complementaryEducation
+         {complementaryEducation
           .slice(currentCertIndex, currentCertIndex + 3)
           .map((cert, idx) => (
             <div
