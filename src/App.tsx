49e3087782d5d38041b9231376a0403d8c8413f9
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-15 17:21:01 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -94,6 +94,7 @@ function App() {
         projects: "Proyectos",
         experience: "Experiencia",
         education: "Educación",
+        certification: "Certificaciones".
         contact: "Contacto"
       },
       hero: {
