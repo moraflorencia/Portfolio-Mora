de714e5e6cd9e964c239f4d024b8d471964f4285
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-15 15:52:44 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import {
   Award, Briefcase, Send, Calendar, GraduationCap, X, 
   ZoomIn, Moon, Sun, Menu, Globe, Star, Sparkles 
 } from 'lucide-react';
-
 import { Languages } from 'lucide-react';
 import { FaWhatsapp } from "react-icons/fa";
 import BackgroundEffects from './components/BackgroundEffects';
