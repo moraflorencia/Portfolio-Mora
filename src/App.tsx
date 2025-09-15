3b31d2b0a4b8866af191a4b8b6ce54cc47652c0d
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-15 16:09:41 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1408,7 +1408,8 @@ export default function CertificationsCarousel({ isDarkMode, t }: any) {
         </div>
       </div>
     </section>
-
+  );
+}
       
       {/* Contact Section */}
       <section id="contact" className="py-20 px-6 relative">
