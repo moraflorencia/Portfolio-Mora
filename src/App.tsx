6a6c503f9ecfaeb71b6d1f073b6048035a98004f
Florencia Mora <mora.florencia.m@gmail.com>
2025-09-19 09:22:56 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -407,7 +407,7 @@ const projects = [
   style={{ 
     backgroundImage: isDarkMode
       ? "url('https://raw.githubusercontent.com/moraflorencia/Portfolio-Mora/refs/heads/main/public/assets/FondoNoche.png')" // fondo oscuro
-      : "url('https://raw.githubusercontent.com/moraflorencia/Portfolio-Mora/refs/heads/main/public/assets/FondoDia.png')", // fondo claro
+      : "url('https://raw.githubusercontent.com/moraflorencia/Portfolio-Mora/refs/heads/main/public/assets/ChatGPT%20Image%2031%20ago%202025%2C%2021_46_28.pngg')", // fondo claro
     backgroundSize: "cover",
     backgroundPosition: "center",
     backgroundAttachment: "fixed"
