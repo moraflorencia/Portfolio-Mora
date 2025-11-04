113bc61456f1b2fc53d7bef61efa3fb5a0b1f276
Florencia Mora <mora.florencia.m@gmail.com>
2025-11-04 19:58:30 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import TypewriterEffect from './components/TypewriterEffect';
 import BackgroundEffects from './components/BackgroundEffects';
 import { FormEmail } from './components/FormEmail'; 
 import { HiBuilding } from 'react-icons/hi';
+import { DatabaseIcon } from '@heroicons/react/outline'; 
 
 // Hook para efecto máquina de escribir
 const useTypewriter = (words: string[], speed = 150, delay = 2000) => {
@@ -267,7 +268,7 @@ function App() {
     },
     {
      name: 'SAP',
-     icon: Brain,
+     icon: DatabaseIcon,
      items: ['ABAP'],
      color: isDarkMode ? 'from-white/5 to-white/20' : 'from-white/50 to-rose-200/30',
 }
